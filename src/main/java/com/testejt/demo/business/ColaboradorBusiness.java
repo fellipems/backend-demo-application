bfa2b40918dcccf9efafb3609d6ff03f2d7e6c48
Fellipe Martins de Sousa <fellipems14@gmail.com>
2023-08-29 21:24:51 -0300

    feat(ColaboradorBusiness): add RabbitTemplate for queue, when save colaborador, send the password and score by queue for consuming in RabbitMq Service

--- src/main/java/com/testejt/demo/business/ColaboradorBusiness.java
+++ src/main/java/com/testejt/demo/business/ColaboradorBusiness.java
@@ -1,22 +1,11 @@
 package com.testejt.demo.business;
 
 import com.testejt.demo.dto.ChefeSubordinado;
-import com.testejt.demo.entity.Colaborador;
+import com.backend.entity.Colaborador;
 import com.testejt.demo.repository.ColaboradorRepository;
-import com.testejt.demo.util.SenhaUtil;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import javax.crypto.Cipher;
-import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.PBEKeySpec;
-import javax.crypto.spec.SecretKeySpec;
-import java.nio.charset.StandardCharsets;
-import java.security.spec.KeySpec;
-import java.util.Base64;
-import java.util.Optional;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
 
 import java.util.List;
 
@@ -25,6 +14,9 @@ public class ColaboradorBusiness {
 
     @Autowired
     ColaboradorRepository colaboradorRepository;
+
+    @Autowired
+    RabbitTemplate rabbitTemplate;
 
     public Colaborador findById(Integer id) throws Exception {
         return colaboradorRepository.findById(id).orElseThrow(() -> new Exception("nenhum colaborador encontrado"));
@@ -35,10 +27,14 @@ public class ColaboradorBusiness {
     }
 
     public Colaborador save(Colaborador colaborador) {
-        colaborador.setScore(SenhaUtil.calculaComplexidade(colaborador.getSenha()));
-        colaborador.setSenha(SenhaUtil.encryptPassword(colaborador.getSenha()));
+//        colaborador.setScore(SenhaUtil.calculaComplexidade(colaborador.getSenha()));
+//        colaborador.setSenha(SenhaUtil.encryptPassword(colaborador.getSenha()));
 
-        return colaboradorRepository.save(colaborador);
+        colaborador = colaboradorRepository.save(colaborador);
+
+        rabbitTemplate.convertAndSend("spring-boot-exchange", "foo.bar.baz", colaborador.getId());
+
+        return colaborador;
     }
 
     public Colaborador associaSubordinado(ChefeSubordinado chefeSubordinado) {
