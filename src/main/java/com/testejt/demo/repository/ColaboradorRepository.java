389ae2ca05df35075b9e0f445b7d8a1866d5d72f
Fellipe Martins de Sousa <fellipems14@gmail.com>
2023-08-29 21:26:26 -0300

    style(ColaboradorRepository): rename import

--- src/main/java/com/testejt/demo/repository/ColaboradorRepository.java
+++ src/main/java/com/testejt/demo/repository/ColaboradorRepository.java
@@ -1,6 +1,6 @@
 package com.testejt.demo.repository;
 
-import com.testejt.demo.entity.Colaborador;
+import com.backend.entity.Colaborador;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
